e1192eb9cf817f2303d70f36c070b964b95f1382
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-14 09:15:27 +0100

    sl_SI
    Slovenian language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+
+// Commmon
+$lang['Share'] = 'Delite';
+$lang['This album is shared via a public link'] = 'Ta album je v skupni rabi prek javne povezave';
+$lang['Copy to clipboard'] = 'Kopiraj v odložišče';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Povezava je bila uspešno kopirana v odložišče. Zdaj lahko uporabite sistemsko funkcijo lepljenja, da jo delite!';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Povezava ni bila kopirana v odložišče. Vaš brskalnik morda ne podpira te funkcije.';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Prosimo izberite povezavo in uporabite funkcijo Urejanje > Kopiraj v brskalniku.';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Obnovili boste skupno povezavo za ta album. Prej posredovana povezava ne bo več aktivna. Ali ste prepričani?';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Ali ste prepričani, da želite preklicati skupno rabo tega albuma?';
+$lang['Share this album'] = 'Delite ta album';
+$lang['Renew link'] = 'Obnovi povezavo';
+$lang['Cancel sharing'] = 'Prekliči skupno rabo';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Povezava za skupno rabo je bila uspešno ustvarjena. Kliknite gumb za skupno rabo, da ga prikažete.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Povezava je bila uspešno obnovljena. Kliknite gumb za skupno rabo, da ga prikažete.';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Povezava je bila uspešno izbrisana. Album ni več v javni skupni rabi.';
+// Admin
+$lang['Hide menus for albums visitors'] = 'Skrij menije za obiskovalce albuma';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Ko je potrjeno, so meniji skriti za obiskovalce skupnega albuma';
+$lang['Replace navigation breadcrumbs with album name'] = 'Zamenjajte navigacijske drobtine (breadcrumbs) z imenom albuma';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Ko je izbrano, se drobtine (breadcrumbs) nadomestijo z imenom albuma';
+$lang['Show a login menu'] = 'Pokaži meni za prijavo';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Ko je potrjeno, je prikazan meni za prijavo za goste, ki brskajo prek povezave v skupni rabi';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'S tem boste prekinili povezavo s trenutnim albumom in naložili stran za prijavo. Ali ste prepričani?';
+$lang['Last visit'] = 'Zadnji obisk';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Nastavi piškotek zapomni si me za prijavljene goste (samodejna prijava)';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Ko je potrjeno, se lahko uporabniki vrnejo na korenski URL Piwigo in se samodejno prijavijo za brskanje po zadnjem obiskanem albumu v skupni rabi';
+$lang['IP address'] = 'IP naslov';
+$lang['Visit date'] = 'Datum obiska';
+$lang['Connection log for selected album'] = 'Dnevnik povezav za izbrani album';
+$lang['Activity logs shown below'] = 'Dnevniki dejavnosti so prikazani spodaj';
+$lang['Please choose an action : '] = 'Prosimo, izberite dejanje: ';
+$lang['Shared link'] = 'Povezava v skupni rabi';
+$lang['Share a new album'] = 'Dajte v skupno rabo nov album';
+$lang['Active shares'] = 'Albumi v skupni rabi';
+$lang['No shared album'] = 'Ni albuma v skupni rabi';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Ni zasebnega albuma za skupno rabo (ali ki še ni v skupni rabi)';
+$lang['Show links'] = 'Pokaži povezave';
+$lang['Ascending'] = 'Naraščajoče';
+$lang['Descending'] = 'Padajoče';
+$lang['Number of displayed images per page for shared albums'] = 'Število prikazanih slik na strani za albume v skupni rabi';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Določa največje število slik za prikaz na strani za albume v skupni rabi. Ta nastavitev velja za vse albume v skupni rabi';
+$lang['Shared by'] = 'V skupno rabo dal';
+$lang['Advanced options'] = 'Napredne možnosti';
+$lang['Enable non-admin powerusers to share albums'] = 'Omogoči uporabnikom, ki niso skrbniki, da delijo albume';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = 'Ko je označeno, ima vsak uporabnik, ki pripada skupini \'sharealbum_powerusers\' dostop do možnosti skupne rabe na straneh zasebnih albumov';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'Za to dodajte napredne uporabnike v skupino <b>\'sharealbum_powerusers\'</b>.';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = 'Opozorilo : ti uporabniki bodo lahko ustvarili albume v skupni rabi in odstranili obstoječe skupne rabe';
+$lang['Apply shares to sub-albums']='Uporabi skupne rabe za podalbume';
+$lang['When checked, nested albums are shared as well as the parent shared album']='Ko je označeno, lahko brskate tudi po ugnezdenih albumih';
+
+$lang['Navigation options'] = 'Navigacijske možnosti';
+$lang['Shared albums options'] = 'Možnosti albumov v skupni rabi';
+$lang['Go'] = 'Pojdi';
+$lang['Apply'] = 'Uporabi';
+$lang['Shared Albums'] = 'Albumi v skupni rabi';
+?>
